dedd7752aa4fed786f9a6ff3a56247632baefe59
aureliano-yepez <aureliano.yepez@alectio.com>
2020-08-11 09:32:31 -0700

    raise exceptions for invalid fields and fields that are not found

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -8,13 +8,15 @@ from alectio.exceptions import YAMLFieldNotFound, InvalidYAMLField, InvalidYAMLF
 from alectio.tools.parser import ParseStrategyYaml
 
 
+
 @pytest.fixture
 def original_yaml_paths():
     pwd = os.getcwd()
     paths = [
-        f"{pwd}/test_files/vgg_16_original.py",
-        f"{pwd}/test_files/vgg_16_modified.py",
-        f"{pwd}/test_files/alex_net.py"
+        f"{pwd}/test_files/invalid_yamls/invalid_outer_field_not_found.yaml",
+        f"{pwd}/test_files/invalid_yamls/invalid_simple_qs_field_not_found.yaml",
+        f"{pwd}/test_files/invalid_yamls/invalid_simple_qs_field.yaml",
+        f"{pwd}/test_files/invalid_yamls/invalid_simple_qs_field_type.yaml"
     ]
     # map over and eppend pwd
     return paths
@@ -24,42 +26,28 @@ def test_outer_field_not_found(original_yaml_paths):
     file = original_yaml_paths[0]
     with pytest.raises(YAMLFieldNotFound):
         # original_model.update_model(new_model)
-        yaml_object = ParseStrategyYaml(file)
- 
+        ParseStrategyYaml(file)
     return 
-
-def test_outer_field_not_found(original_yaml_paths):
-    file = original_yaml_paths[0]
-    with pytest.raises(YAMLFieldNotFound):
-        # original_model.update_model(new_model)
-        yaml_object = ParseStrategyYaml(file)
- 
-    return 
-
-
 
 def test_qs_field_not_found(original_yaml_paths):
-    file = original_yaml_paths[0]
-    with pytest.raises(InvalidYAMLField):
+    file = original_yaml_paths[1]
+    with pytest.raises(YAMLFieldNotFound):
         # original_model.update_model(new_model)
-        yaml_object = ParseStrategyYaml(file)
-
+        ParseStrategyYaml(file)
     return 
 
-
-def test_invalid_qs_fields(original_yaml_paths):
-    file = original_yaml_paths[0]
+def test_invalid_type_qs_fields(original_yaml_paths):
+    file = original_yaml_paths[2]
     with pytest.raises(InvalidYAMLField):
         # original_model.update_model(new_model)
         ParseStrategyYaml(file)
-
- 
     return 
 
-
-def test_invalid_type_qs_fields(original_yaml_paths):
-    file = original_yaml_paths[0]
+def test_invalid_qs_fields(original_yaml_paths):
+    file = original_yaml_paths[3]
     with pytest.raises(InvalidYAMLFieldType):
         # original_model.update_model(new_model)
         ParseStrategyYaml(file)
     return 
+
+
