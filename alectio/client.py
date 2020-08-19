1bdd9416eff49f355361dd51438d383e16d600bb
Devi Tripathy <devi.tripathy@alectio.com>
2020-08-19 13:24:26 -0700

    OAuth added, Create project and create experiments added

--- alectio/client.py
+++ alectio/client.py
@@ -3,6 +3,10 @@ import os
 import json
 import asyncio
 import sys
+import uuid
+import yaml
+
+from datetime import datetime
 
 from gql import Client, gql
 from aiogqlc import GraphQLClient
@@ -32,11 +36,14 @@ class AlectioClient:
             raise APIKeyNotFound
 
         self._api_key = self._environ['ALECTIO_API_KEY']
+        self._client_secret = self._environ['CLIENT_SECRET']
+        self._client_id = self._environ['CLINET_ID']
+        self._client_token = None
 
         # cli user settings
         self._settings = {
             'git_remote': "origin",
-            # 'base_url': "https://api.alectio.com"
+            #'base_url': "https://api.alectio.com"
             'base_url': "http://localhost:5005"
         }
 
@@ -55,10 +62,31 @@ class AlectioClient:
         # client to upload files, images, etc. 
         # uses https://pypi.org/project/aiogqlc/
         self._upload_client = GraphQLClient('http://localhost:5005/graphql')
-
+        self._oauth_server = 'http://localhost:5000/'
         # need to retrive user_id based on token @ DEVI from OPENID 
-        self._user_id = "82b4fb909f1f11ea9d300242ac110002" # ideally this should be set already 
+        self._user_id = "82b4fb909f1f11ea9d300242ac110002" # ideally this should be set already. Dummy one will be set when init is invoked
         # compnay id = 7774e1ca972811eaad5238c986352c36
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    def request_client_token(self):
+        pass
+
+    def init(self, file_path=None):
+        if file_path and self._client_token is None:
+            with open(file_path, 'r') as f:
+                self._client_token = json.load(f)
+            headers = {"Authorization": "Bearer " + self._client_token['access_token']}
+            requests_data = requests.get(
+                url=self._oauth_server + 'api/me', headers=headers)
+            if requests_data.status_code == 401:
+                print("Clinet Token Expired. Fetching new one.")
+                self.request_client_token()
+            elif requests_data.status_code == 200:
+                requests_data = requests_data.json()
+                self._user_id = requests_data['id']
+                print(self._user_id)
+
+        #TODO: Create the token request if not present and save it to the disk
 
     def get_single(self, resource, query_string, params):
         """
@@ -76,6 +104,21 @@ class AlectioClient:
            hash_key = extract_id(singular['sk'])
         singular_object = class_to_init(self._client, singular, self._user_id, hash_key)
         return singular_object
+
+
+    def mutate_single(self, query_string, params):
+        """
+        return a single object for the requested resource.
+        :params: resource - name of the resource to obtain i.e experiments, projects, models, etc.
+        :params: query_string - graphql string to invoke.
+        :params: params - variables required to invoke query string in the client.
+        """
+        query = gql(query_string)
+        class_name = lambda class_name: getattr(sys.modules[__name__], class_name)
+        singular = self._client.execute(query, params)
+        print(singular)
+        return True
+
 
     def get_collection(self, resource, query_string, params):
         """
@@ -178,19 +221,38 @@ class AlectioClient:
 
         return None 
 
-    # TODO:
+
     def create_project(self, file):
         """
         create user project 
         """
-        return Project("", "", "", "")
+        with open(file, 'r') as yaml_in:
+            yaml_object = yaml.safe_load(yaml_in) # yaml_object will be a list or a dict
+            project_dict = yaml_object['Project']
+            project_dict['userId'] = self._user_id
+            now = datetime.now()
+            project_dict['date'] = now.strftime("%m-%d-%Y") # We probably should not allow cli to decide DATE
+            print(project_dict)
+            params = project_dict
+            response =  self.mutate_single(PROJECT_CREATE_FRAGMENT, params)
+        
+        return "Done"
 
-    # TODO:
     def create_experiment(self, file):
         """
         create user experient
         """
-        return Experiment("", "", "", "")
+        with open(file, 'r') as yaml_in:
+            yaml_object = yaml.safe_load(yaml_in) # yaml_object will be a list or a dict
+            experiment_dict = yaml_object['Experiment']
+            experiment_dict['experimentId'] = "".join(str(uuid.uuid1()).split("-"))
+            experiment_dict['userId'] = self._user_id
+            now = datetime.now()
+            experiment_dict['date'] = now.strftime("%m-%d-%Y")
+            params = experiment_dict
+            response =  self.mutate_single(EXPERIMENT_CREATE_FRAGMENT, params)
+
+        return "Done"
 
     # TODO:
     def create_model(self, model_path):
